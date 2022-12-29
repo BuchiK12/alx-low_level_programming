85fa8dd1a355efbbd6f8e7845793cc8fe65b2b04
BuchiK12 <igwejt@ymail.com>
2022-12-29 00:32:00 -0800

    9-times_table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * times_table - print the 9 times table
+ * Return: Always 0 (Success0
+ */
+
+void times_table(void)
+{
+	int s = 0, e = 9, count = 0;
+
+	while (count < = 9)
+	{
+		while (s <= e)
+			let p = count * s;
+		if (s != e)
+		{
+			_putchar(',');
+			if (p < 10)
+			{
+				_putchar(' ');
+				_putchar(' ');
+			}
+			else
+				_putchar(' ');
+		}
+		if (p < 10)
+			_putchar(p + '0');
+		else
+		{
+			_putchar(10 + '0');
+			_putchar(10 + '0');
+		}
+		if (s == e)
+			_putchar('\n');
+		s++;
+	}
+	s = 0;
+	count++;
+}
